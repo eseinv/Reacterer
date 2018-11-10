c90fb5cae1f5686f94e6d6365f50583f21e63df6
Ievgen Sein <xdiego.pwu@gmail.com>
2018-11-10 21:32:35 +0200

    a

--- src/components/change-score.js
+++ src/components/change-score.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
 export default function ChangeScore(props) {
   function handleClick() {
     let actn = props.action;
-    if (actn == "less") {
+    if (actn === "less") {
       var num = -1;
-    } else if (actn == "more") {
-      var num = 1;
+    } else if (actn === "more") {
+      num = 1;
     }
     props.countFunc(num);
   }
