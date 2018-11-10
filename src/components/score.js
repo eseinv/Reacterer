756a8243d41b6ce85ec3ad724cb301785d73e153
Ievgen Sein <xdiego.pwu@gmail.com>
2018-11-10 23:13:46 +0200

    a

--- src/components/score.js
+++ src/components/score.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function Score(props) {
+  function handleClick() {
+    props.reset();
+  }
   return (
-    <div className="text-center text-primary h2">
-      {" "}
-      Current count is: {props.score}{" "}
+    <div
+      className="text-center text-primary h2 mb-3 hand"
+      onClick={handleClick}
+    >
+      Current count is: {props.score}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,16 +9,21 @@ class App extends Component {
 
     this.state = { count: 0 };
     this.changeCount = this.changeCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
   }
 
   changeCount(actn) {
     this.setState({ count: this.state.count + actn });
   }
 
+  resetCount() {
+    this.setState({ count: 0 });
+  }
+
   render() {
     return (
       <div>
-        <Score score={this.state.count} />
+        <Score score={this.state.count} reset={this.resetCount} />
         <ChangeScore action="more" countFunc={this.changeCount} />
         <ChangeScore action="less" countFunc={this.changeCount} />
       </div>
