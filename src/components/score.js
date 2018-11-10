2166868c2417142234a690f475f911faaf2482ad
Ievgen Sein <xdiego.pwu@gmail.com>
2018-11-10 20:54:09 +0200

    a

---
+++ src/components/score.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+export default function Score(props) {
+  return <div> Current count is: {props.score} </div>;
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Score from "./components/score";
+import ChangeScore from "./components/change-score";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 0 };
+    this.changeCount = this.changeCount.bind(this);
+  }
+
+  changeCount(actn) {
+    this.setState({ count: this.state.count + actn });
+  }
+
+  render() {
+    return (
+      <div>
+        <Score score={this.state.count} />
+        <ChangeScore action="less" countFunc={this.changeCount} />
+        <ChangeScore action="more" countFunc={this.changeCount} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
