d12cb244741a1faad314b62f8076255d13df37b7
pbeocanin <pbeocanin@gmail.com>
2018-11-12 07:06:56 +0100

    Refactor
    You can use `{props.children}` or `{this.props.children}` to render whatever is between <Component> and </Component>, so you can wrap stuff

--- src/components/change-score.js
+++ src/components/change-score.js
@@ -2,28 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ChangeScore = props => {
-  const handleClick = () => {
-    const amnt = props.action;
-    let num;
-
-    if (amnt === "less") {
-      num = -1;
-    } else if (amnt === "more") {
-      num = 1;
-    }
-    props.countFunc(num);
-  };
-
-  const nameButton = () => {
-    return props.action === "less" ? "Decrease" : "Increase";
-  };
-
   return (
-    <div className="text-center">
-      <button className="btn btn-info my-1 bstyle" onClick={handleClick}>
-        {nameButton()}
-      </button>
-    </div>
+    <button
+      className={`btn m-1 ${
+        props.action === "less" ? "btn-secondary" : "btn-danger"
+      }`}
+      onClick={() => {
+        props.countFunc(props.action === "less" ? -1 : 1);
+      }}
+    >
+      {props.children}
+    </button>
   );
 };
 
