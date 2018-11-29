6325b4c6c72771cb262265bfa9de86f4a138dc71
pbeocanin <pbeocanin@gmail.com>
2018-11-29 01:41:38 +0100

    Updated figureBorderRadius

--- src/pages/frouta/product-list.js
+++ src/pages/frouta/product-list.js
@@ -18,18 +18,24 @@ const H5 = styled.h5`
 const P = styled.p`
 	color: #707070;
 `;
-
-function figureBorderRadius(i) {
-	let border;
+/* There's a few problems here.
+1: no need for 'fucntion' keyword. It's bad practice mostly cuz you mispell it
+2: You're checking 3 conditions when you should be checking two. Check what I did
+3: Really important - always use arrow function unless you specifically need to
+use `this` INSIDE the function, which shouldn't be THIS from the scope you're in.
+It's almost never, so simply always use arrow functions
+4: UI/UX wise, I don't think you should round products in a row differently.
+Try adding 4 rows it'll look bad.
+*/
+const figureBorderRadius = i => {
 	if (i === 0) {
-		border = '15px 5px 5px 15px';
-	} else if (i > 0 && i < ProductList.length - 1) {
-		border = '5px 5px 5px 5px';
-	} else if (i === ProductList.length - 1) {
-		border = '5px 15px 15px 5px';
+		return '15px 5px 5px 15px';
 	}
-	return border;
-}
+	if (i === ProductList.length - 1) {
+		return '5px 15px 15px 5px';
+	}
+	return '5px 5px 5px 5px';
+};
 
 const ProductList = MainProductList.map((product, index) => (
 	<div key={index} className="col-4 text-justify">
@@ -46,5 +52,14 @@ const ProductList = MainProductList.map((product, index) => (
 		</ProductBorder>
 	</div>
 ));
+
+/*
+	One cool thing you can pass is a bool prop. Check this out:
+	<ProductBorder className="card" i={i = ProductList.length - 1}>
+	This will set props.i to TRUE when the condition is true, else {
+	it'll be false. In this specific case, no need to use it, but it would
+	eliminate the need for a function
+	}
+*/
 
 export { ProductList };
