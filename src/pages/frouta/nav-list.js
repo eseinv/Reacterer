355ba537e9e78ddeb8c7923ffb3409bac3f957fb
pbeocanin <pbeocanin@gmail.com>
2018-11-29 22:03:20 +0100

    Routes

--- src/pages/frouta/nav-list.js
+++ src/pages/frouta/nav-list.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Button = styled.div`
+const Button = styled(Link)`
 	position: relative;
 	&:after {
 		content: '';
@@ -10,7 +11,8 @@ const Button = styled.div`
 		left: 0;
 		width: 100%;
 		height: 3px;
-		background: ${props => (props.underline ? 'white' : 'transparent')};
+		background: ${props =>
+			props.underline === 'true' ? 'white' : 'transparent'};
 	}
 	&:hover {
 		color: #fff;
@@ -18,19 +20,27 @@ const Button = styled.div`
 `;
 
 const Current = styled.span`
-	color: ${props => (props.is ? '#fff' : '#eee')};
-	font-size: ${props => (props.is ? '105%' : '100%')};
+	color: ${props => (props.active ? '#fff' : '#eee')};
+	font-size: ${props => (props.active ? '105%' : '100%')};
 	&:hover {
-		color: ${props => (props.is ? '#fff' : '#fff')};
+		color: ${props => (props.active ? '#fff' : '#fff')};
 	}
 `;
 
-const NavItems = ['Αρχική', 'Τιμοκατάλογος', 'Επικοινωνία'];
+const NavItems = [
+	{ name: 'Αρχική', link: '/frouta/1' },
+	{ name: 'Τιμοκατάλογος', link: '/frouta/2' },
+	{ name: 'Επικοινωνία', link: '/frouta/3' },
+];
 
-const NavLi = NavItems.map((NavItem, index) => (
+const NavLi = NavItems.map((item, index) => (
 	<div key={index} className="d-flex h-100 align-items-center">
-		<Button className="ml-3 btn" underline={index === 0}>
-			<Current is={index === 0}>{NavItem}</Current>
+		<Button
+			className="ml-3 btn"
+			underline={index === 0 ? 'true' : 'false'}
+			to={item.link}
+		>
+			<Current active={index === 0}>{item.name}</Current>
 		</Button>
 	</div>
 ));
