7a811e4db567950613bd70c5b592495d06d261d3
pbeocanin <pbeocanin@gmail.com>
2018-11-29 22:19:22 +0100

    Added login to button

--- src/pages/frouta/nav-actions.js
+++ src/pages/frouta/nav-actions.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import { SignInAltSolid } from './icons/sign-in-alt-solid';
+
+const Button = styled.div`
+	color: #373535;
+	&:hover {
+		color: #fff;
+	}
+`;
 
 const NavActions = props => {
 	if (props.user) {
-		return <SignInAltSolid color="#373535" />;
+		return (
+			<Button>
+				<SignInAltSolid color="currentColor" />
+				<span>Log in</span>
+			</Button>
+		);
 	}
 	return <SignInAltSolid color="#373535" />;
 };
