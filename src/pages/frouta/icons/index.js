f5b600abf09fac8cd99e33e17247baa5b7fa9e48
pbeocanin <pbeocanin@gmail.com>
2018-11-29 01:35:31 +0100

    SVG Updates

---
+++ src/pages/frouta/icons/index.js
@@ -0,0 +1,5 @@
+/* this is for pure convenience. So you can import {whatever} from './icons'
+	instead of targeting file names */
+
+export { SignInAltSolid } from './sign-in-alt-solid';
+export { ShoppingCartSolid } from './shopping-cart-solid';
